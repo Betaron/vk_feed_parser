fa4049797fc11e733e95077b0b62d6b3e34c6f7e
Иван Агабалаев <61901199+Betaron@users.noreply.github.com>
2021-02-26 15:13:58 +0300

    create new class for work with UI

---
+++ vk_feed_parser/UIWorker.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace vk_feed_parser
+{
+	class UIWorker
+	{
+	}
+}
