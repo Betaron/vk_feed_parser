02b22fbd752f2e9f01a238add934cb9c7a480fb3
Иван Агабалаев <61901199+Betaron@users.noreply.github.com>
2021-03-02 10:48:02 +0300

    add class for parsiing

---
+++ vk_feed_parser/Parser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using VkNet;
+using VkNet.Model;
+using VkNet.Enums.Filters;
+using System.Threading;
+namespace vk_feed_parser
+{
+	class Parser
+	{
+		
+	}
+}
