7018bae34f07375d4f53299cbbb4bbb8a88a53a0
Иван Агабалаев <61901199+Betaron@users.noreply.github.com>
2021-03-03 19:39:52 +0300

    add methods for read and write  json files

--- vk_feed_parser/FileWorker.cs
+++ vk_feed_parser/FileWorker.cs
@@ -1,10 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿
+using System.IO;
+using Newtonsoft.Json;
 
 namespace vk_feed_parser
 {
-	class FileWorker
+	static class FileWorker
 	{
+		public static void SaveToJsonFile(string path, object item, Formatting options = Formatting.Indented)
+		{
+			File.WriteAllText(path, JsonConvert.SerializeObject(item, options));
+		}
+
+		public static TItem LoadFromJsonFile<TItem>(string path, JsonSerializerSettings settings = null)
+		{
+			return JsonConvert.DeserializeObject<TItem>(File.ReadAllText(path), settings);
+		}
 	}
 }
