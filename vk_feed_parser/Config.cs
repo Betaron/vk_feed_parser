80e746fd02a0351d75d8b37c63277397a3e2d61d
Иван Агабалаев <61901199+Betaron@users.noreply.github.com>
2021-03-03 19:41:25 +0300

    Add Config class with its functionality

---
+++ vk_feed_parser/Config.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+
+namespace vk_feed_parser
+{
+	public class Config
+	{
+		public ulong appId { get; private set; }
+		public string token { get; set; }
+		public bool IsStayOnline { get; set; }
+
+		public Config()
+		{
+			SetDefaultConfig();
+		}
+
+		public void SetDefaultConfig()
+		{
+			appId = 0;
+			IsStayOnline = false;
+			token = string.Empty;
+		}
+
+		public void SetConfig(Config externalConfig)
+		{
+			appId = externalConfig.appId;
+			IsStayOnline = externalConfig.IsStayOnline;
+			token = externalConfig.token;
+		}
+
+		public void WriteConfig(Config externalConfig) =>
+			FileWorker.SaveToJsonFile(Path.Combine(Directory.GetCurrentDirectory(), "config.json"), externalConfig);
+
+		public void ReadConfig()
+		{
+			string path = Path.Combine(Directory.GetCurrentDirectory(), "config.json");
+			try
+			{
+				Config externalConfig = FileWorker.LoadFromJsonFile<Config>(path);
+				SetConfig(externalConfig);
+			}
+			catch
+			{
+				SetDefaultConfig();
+				WriteConfig(this);
+			}
+		}
+	}
+}
