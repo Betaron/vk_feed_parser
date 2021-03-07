cf9b47c06d75e783ecb682e37a83de85254b907d
Иван Агабалаев <61901199+Betaron@users.noreply.github.com>
2021-03-07 21:18:32 +0300

    Add Class with datasheets structures

---
+++ vk_feed_parser/PostData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace vk_feed_parser
+{
+	class PostData
+	{
+		public struct TextData
+		{
+			public ulong? postID;
+			public string postText;
+		}
+		
+		public struct ImagesData
+		{
+			public ulong? postID;
+			public string[] postImages; 
+		}
+
+		public struct LinksData
+		{
+			public ulong? postID;
+			public string[] postLinks;
+		}
+	}
+}
