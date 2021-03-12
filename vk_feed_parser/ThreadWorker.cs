3736c7856c817434b554e64bf85c7fa7d4389629
Иван Агабалаев <61901199+Betaron@users.noreply.github.com>
2021-03-12 19:50:19 +0300

    add functionality in method, which return separateing thread

--- vk_feed_parser/ThreadWorker.cs
+++ vk_feed_parser/ThreadWorker.cs
@@ -3,27 +3,45 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading;
 using VkNet.Model;
+using System.Linq;
 
 namespace vk_feed_parser
 {
 	public class ThreadWorker
 	{
 		Queue<NewsItem> posts;
-		static object queueGetLocker = new object();
+		static object getQueueLocker = new object();
 
 		public ThreadWorker(IEnumerable<NewsItem> posts)
 		{
 			this.posts = new Queue<NewsItem>(posts);
 		}
 
-		public Thread GetPackingThread()
+		public Thread GetPackingThread<TData>(
+			object locker, 
+			string path, 
+			Func<NewsItem, TData> SeparateData, 
+			List<TData> dataList
+			)
 		{
 			return new Thread(() =>
 			{
+				const uint range = 10;
+				var bufData = new List<TData>();
 				Queue<NewsItem> localPosts;
-				lock (queueGetLocker)
+				lock (getQueueLocker)
 				{
 					localPosts = posts;
+				}
+				while (localPosts.Count != 0)
+				{
+					lock (locker)
+					{
+						for (int i = 0; i < range; i++)
+							bufData.Add(SeparateData(localPosts.Dequeue()));
+						var externalData = FileWorker.LoadFromJsonFile<List<TData>>(path);
+						dataList.AddRange(bufData.Except(externalData));
+					}
 				}
 			});
 		}
