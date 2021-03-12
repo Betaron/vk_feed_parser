327a11570c356c9d458c35889e3eb8b47b02f31e
Иван Агабалаев <61901199+Betaron@users.noreply.github.com>
2021-03-12 18:07:07 +0300

    Add method, which can get a posts array

--- vk_feed_parser/ThreadWorker.cs
+++ vk_feed_parser/ThreadWorker.cs
@@ -2,11 +2,30 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading;
+using VkNet.Model;
 
 namespace vk_feed_parser
 {
 	public class ThreadWorker
 	{
+		Queue<NewsItem> posts;
+		static object queueGetLocker = new object();
 
+		public ThreadWorker(IEnumerable<NewsItem> posts)
+		{
+			this.posts = new Queue<NewsItem>(posts);
+		}
+
+		public Thread GetPackingThread()
+		{
+			return new Thread(() =>
+			{
+				Queue<NewsItem> localPosts;
+				lock (queueGetLocker)
+				{
+					localPosts = posts;
+				}
+			});
+		}
 	}
 }
